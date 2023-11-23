f4d9c774b98f7e18f7eefb39441305e00197da9e
ly2011 <1518550424@qq.com>
2023-11-23 22:35:01 +0800

    feat: 删除冗余的代码

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,43 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import contactsRouters from './course01/contacts/router';
 import course02Routers from './course02/query-page/router';
 import course03Routers from './course03/todos/router';
-// import Root, { loader as rootLoader, action as rootAction } from './course01/contacts/root';
-// import ErrorPage from './error-page';
-// import Index from './course01/contacts/index';
-// import Contact, { loader as contactLoader } from './course01/contacts/contact';
-// import EditContact, { action as editAction } from './course01/contacts/edit';
-// import { action as destroyAction } from './course01/contacts/destroy';
 
-const router = createBrowserRouter([
-  ...contactsRouters,
-  ...course02Routers,
-  ...course03Routers,
-  // {
-  //   path: '/',
-  //   element: <Root />,
-  //   errorElement: <ErrorPage />,
-  //   loader: rootLoader,
-  //   action: rootAction,
-  //   children: [
-  //     { index: true, element: <Index /> },
-  //     {
-  //       path: 'contacts/:contactId',
-  //       element: <Contact />,
-  //       loader: contactLoader,
-  //     },
-  //     {
-  //       path: 'contacts/:contactId/edit',
-  //       element: <EditContact />,
-  //       loader: contactLoader,
-  //       action: editAction,
-  //     },
-  //     {
-  //       path: 'contacts/:contactId/destroy',
-  //       action: destroyAction,
-  //       errorElement: <div>Oops! There was an error.</div>,
-  //     },
-  //   ],
-  // },
-]);
+const router = createBrowserRouter([...contactsRouters, ...course02Routers, ...course03Routers]);
 
 export default router;
