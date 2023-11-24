464d0020eaab331b686b72732440d180938ed574
ly2011 <1518550424@qq.com>
2023-11-24 21:52:49 +0800

    feat: 隐藏course01内容，防止样式污染

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
-import contactsRouters from './course01/contacts/router';
+// import contactsRouters from './course01/contacts/router';
 import course02Routers from './course02/query-page/router';
 import course03Routers from './course03/todos/router';
 
-const router = createBrowserRouter([...contactsRouters, ...course02Routers, ...course03Routers]);
+const router = createBrowserRouter([...course02Routers, ...course03Routers]);
 
 export default router;
