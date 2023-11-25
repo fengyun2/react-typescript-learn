40481928451b687109f24044165b7c0cb478da54
ly2011 <1518550424@qq.com>
2023-11-25 22:12:17 +0800

    chore: 配置typescript及vite路径别名

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/router';
 
+import '@antd/dist/reset.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <RouterProvider router={router} />
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json', '.sass', '.scss'], // 忽略输入的扩展名
+    alias: [
+      { find: /^~/, replacement: '' },
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '~', replacement: path.resolve(__dirname, './node_modules') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@routes',
+        replacement: path.resolve(__dirname, 'src/routes'),
+      },
+      { find: '@utils', replacement: path.resolve(__dirname, 'utils') },
+      {
+        find: '@antd/dist/reset.css',
+        replacement: path.join(__dirname, 'node_modules/antd/dist/reset.css'),
+      },
+      // { find: 'antd', replacement: path.join(__dirname, 'node_modules/antd/dist/antd.js') },
+      // {
+      //   find: '@ant-design/icons',
+      //   replacement: path.join(__dirname, 'node_modules/@ant-design/icons/dist/index.umd.js')
+      // }
+    ],
+  },
 });
