c30813fff0a337045f4faf396905326a9434e3ca
ly2011 <1518550424@qq.com>
2023-11-29 23:05:21 +0800

    feat: 优化todos样式

--- src/routes/course05/todos/TodoList.tsx
+++ src/routes/course05/todos/TodoList.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Checkbox, Input, Button, Space, Typography, Flex } from 'antd';
+import { Checkbox, Input, Button, Space, Typography, Flex, List } from 'antd';
 import { useAppState } from './store';
 
 const { Text } = Typography;
@@ -17,45 +17,79 @@ function TodoList() {
     setNewItem('');
   };
 
+  // const renderTodos = () => {
+  //   return (
+  //     <ul style={{ textAlign: 'left' }}>
+  //       {todos.map((todo, index) => {
+  //         return (
+  //           <li key={index}>
+  //             <Space style={{ display: 'flex', alignItems: 'center' }}>
+  //               <Checkbox checked={todo.completed} onChange={() => toggleTodo(index)}>
+  //                 <Text type={todo.completed ? 'secondary' : undefined} delete={todo.completed}>
+  //                   {todo.text}
+  //                 </Text>
+  //               </Checkbox>
+  //               <Button
+  //                 size='small'
+  //                 type='link'
+  //                 danger
+  //                 onClick={() => {
+  //                   removeTodo(index);
+  //                 }}
+  //               >
+  //                 x
+  //               </Button>
+  //             </Space>
+  //           </li>
+  //         );
+  //       })}
+  //     </ul>
+  //   );
+  // };
+
   const renderTodos = () => {
     return (
-      <ul style={{ textAlign: 'left' }}>
-        {todos.map((todo, index) => {
-          return (
-            <li key={index}>
-              <Space style={{ display: 'flex', alignItems: 'center' }}>
-                <Checkbox checked={todo.completed} onChange={() => toggleTodo(index)}>
-                  <Text type={todo.completed ? 'secondary' : undefined} delete={todo.completed}>
-                    {todo.text}
-                  </Text>
-                </Checkbox>
-                <Button
-                  size='small'
-                  type='link'
-                  danger
-                  onClick={() => {
-                    removeTodo(index);
-                  }}
-                >
-                  x
-                </Button>
-              </Space>
-            </li>
-          );
-        })}
-      </ul>
+      <List
+        bordered
+        itemLayout='horizontal'
+        dataSource={todos}
+        renderItem={(todo, index) => (
+          <List.Item>
+            <Space style={{ display: 'flex', alignItems: 'center' }}>
+              {<Text style={{ width: '100%' }}>{index + 1}.</Text>}
+              <Checkbox checked={todo.completed} onChange={() => toggleTodo(index)}>
+                <Text type={todo.completed ? 'secondary' : undefined} delete={todo.completed}>
+                  {todo.text}
+                </Text>
+              </Checkbox>
+              <Button
+                size='small'
+                type='link'
+                danger
+                onClick={() => {
+                  removeTodo(index);
+                }}
+              >
+                x
+              </Button>
+            </Space>
+          </List.Item>
+        )}
+      ></List>
     );
   };
 
   return (
     <Flex justify='center' align='center' vertical>
-      <Space style={{ display: 'flex', alignItems: 'center' }}>
-        <Input value={newItem} onChange={onTodoChange} onPressEnter={onAddTodo} />
-        <Button type='primary' disabled={!newItem} onClick={onAddTodo}>
-          Add
-        </Button>
+      <Space direction='vertical' align='center'>
+        <Space style={{ display: 'flex', alignItems: 'center' }}>
+          <Input value={newItem} onChange={onTodoChange} onPressEnter={onAddTodo} />
+          <Button type='primary' disabled={!newItem} onClick={onAddTodo}>
+            Add
+          </Button>
+        </Space>
+        <div>{renderTodos()}</div>
       </Space>
-      <div>{renderTodos()}</div>
     </Flex>
   );
 }
